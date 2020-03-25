26315c3aa5569e14681452cf892c41a412b4cd67
LongQue <1220756253@qq.com>
2020-03-26 00:45:47 +0800

    09.双栈实现队列

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -124,11 +124,72 @@ func buildTree(preorder []int, inorder []int) (Head *TreeNode) {
 
 	return
 }
+
+/**
+09.用两个栈实现队列
+用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+示例 1：
+输入：
+["CQueue","appendTail","deleteHead","deleteHead"]
+[[],[3],[],[]]
+输出：[null,null,3,-1]
+示例 2：
+输入：
+["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+[[],[],[5],[2],[],[]]
+输出：[null,-1,null,null,5,2]
+提示：
+1 <= values <= 10000
+最多会对 appendTail、deleteHead 进行 10000 次调用
+*/
+type CQueue struct {
+	in  []int
+	out []int
+}
+
+func Constructor() CQueue {
+	return CQueue{
+		make([]int, 0, 5),
+		make([]int, 0, 5),
+	}
+}
+
+func (this *CQueue) AppendTail(value int) {
+	this.in = append(this.in, value)
+}
+
+func (this *CQueue) DeleteHead() int {
+	if len(this.out) == 0 {
+		if len(this.in) == 0 {
+			return -1
+		} else {
+			for len(this.in)!=0 {
+				i := this.in[len(this.in) - 1]
+				this.in=this.in[:len(this.in)-1]
+				this.out = append(this.out, i)
+			}
+		}
+	}
+	i := this.out[len(this.out)-1]
+	this.out = this.out[:len(this.out)-1]
+	return i
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AppendTail(value);
+ * param_2 := obj.DeleteHead();
+ */
 func main() {
-	var preorder = []int{3, 9, 20, 15, 7}
-	var inorder = []int{9, 3, 15, 20, 7}
-	tree := buildTree(preorder, inorder)
-	println(tree.Val)
+	obj := Constructor()
+	obj.AppendTail(3)
+	param := obj.DeleteHead()
+	println(param)
+	param = obj.DeleteHead()
+	println(param)
+	param = obj.DeleteHead()
+	println(param)
 }
 
 //Definition for singly-linked list.
