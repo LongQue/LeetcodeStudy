d6ecfadddf3ee80c6339dff5400d3169db4e5e22
LongQue <1220756253@qq.com>
2020-03-26 23:24:27 +0800

    10-1 斐波那契数列

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -163,9 +163,9 @@ func (this *CQueue) DeleteHead() int {
 		if len(this.in) == 0 {
 			return -1
 		} else {
-			for len(this.in)!=0 {
-				i := this.in[len(this.in) - 1]
-				this.in=this.in[:len(this.in)-1]
+			for len(this.in) != 0 {
+				i := this.in[len(this.in)-1]
+				this.in = this.in[:len(this.in)-1]
 				this.out = append(this.out, i)
 			}
 		}
@@ -176,11 +176,37 @@ func (this *CQueue) DeleteHead() int {
 }
 
 /**
- * Your CQueue object will be instantiated and called as such:
- * obj := Constructor();
- * obj.AppendTail(value);
- * param_2 := obj.DeleteHead();
- */
+10-1斐波那契数列
+写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项。斐波那契数列的定义如下：
+F(0) = 0,   F(1) = 1
+F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+示例 1：
+输入：n = 2
+输出：1
+
+示例 2：
+输入：n = 5
+输出：5
+提示：
+0 <= n <= 100
+
+暂存取代递归
+*/
+func fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	a, b, c := 0, 1, 0
+	for i := 2; i <= n; i++ {
+		c = (a + b)%1000000007
+		a = b
+		b = c
+	}
+	return c
+}
+
 func main() {
 	obj := Constructor()
 	obj.AppendTail(3)
