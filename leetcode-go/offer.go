11d038cf68487e863da9738bc23a4fa30bfde775
LongQue <1220756253@qq.com>
2020-04-01 00:11:57 +0800

    13题，机器人运动范围
    回溯法+判断，从0,0开始遍历四周格子

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -351,11 +351,66 @@ func exist(board [][]byte, word string) bool {
 	}
 	return getPath
 }
+
+/**
+面试题13. 机器人的运动范围
+地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。一个机器人从坐标 [0, 0] 的格子开始移动，它每次可以向左、右、上、下移动一格
+（不能移动到方格外），也不能进入行坐标和列坐标的数位之和大于k的格子。例如，当k为18时，机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。但它不能进入方格 [35, 38]，因为3+5+3+8=19。请问该机器人能够到达多少个格子？
+示例 1：
+输入：m = 2, n = 3, k = 1
+输出：3
+
+示例 2：
+输入：m = 3, n = 1, k = 0
+输出：1
+提示：
+1 <= n,m <= 100
+0 <= k <= 20
+*/
+
+func movingCount(m int, n int, k int) int {
+	tempMap := make([][]bool, m)
+	for k, _ := range tempMap {
+		tempMap[k] = make([]bool, n)
+	}
+
+	//计算位数之和
+	var getDigitSum func(num int) int
+	getDigitSum = func(num int) int {
+		var sum int
+		for num>0{
+			sum+=n%10
+			n=n/10
+		}
+		return sum
+	}
+	//检查是否可用
+	var check func(curM, curN int) bool
+	check = func(curM, curN int) bool {
+		if curM < m && curM >= 0 && curN < n && curN >= 0 && (getDigitSum(curN)+getDigitSum(curM)) <= k && tempMap[curM][curN] != true {
+			return true
+		}
+		return false
+	}
+	//移动到当前
+	var walk func(m,n,count,curM, curN int,tempMap [][]bool) int
+	walk = func(m,n,count,curM, curN int,tempMap [][]bool) int {
+		if check(curM, curN) {
+			tempMap[curM][curN]=true
+			count = 1 + walk(m,n,count,curM-1, curN,tempMap) + walk(m,n,count,curM+1, curN,tempMap) + walk(m,n,count,curM, curN-1,tempMap) + walk(m,n,count,curM, curN+1, tempMap)
+		}
+		return count
+	}
+	count :=walk(m,n,0,0,0,tempMap)
+	return count
+
+}
+
 func main() {
-	board :=[][]byte{
-		{'A','B','C','E'},
-		{'S','F','C','S'},
-		{'A','D','E','E'}}
+	board := [][]byte{
+		{'A', 'B', 'C', 'E'},
+		{'S', 'F', 'C', 'S'},
+		{'A', 'D', 'E', 'E'}}
 	s := "ABCCED"
 	b := exist(board, s)
 	println(b)
