54027a64188e08e5af5cec5b58e6881793a2253d
LongQue <1220756253@qq.com>
2020-04-06 18:42:51 +0800

    面试题22. 链表中倒数第k个节点 双指针，指针A先走k步，然后A,B一起走，A走到末尾，则B走到倒数k

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -774,6 +774,30 @@ func exchange(nums []int) []int {
 	}
 }
 
+/**
+面试题22. 链表中倒数第k个节点
+输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1
+个节点。例如，一个链表有6个节点，从头节点开始，它们的值依次是1、2、3、4、5、6。这个链表的倒数第3个节点是
+值为4的节点。
+
+示例：
+
+给定一个链表: 1->2->3->4->5, 和 k = 2.
+
+返回链表 4->5.
+*/
+func getKthFromEnd(head *ListNode, k int) *ListNode {
+	first, second := head, head
+	for i := 0; i < k; i++ {
+		first = first.Next
+	}
+	for first != nil {
+		first = first.Next
+		second = second.Next
+	}
+	return second
+}
+
 func main() {
 	println(cuttingRope2(120))
 
