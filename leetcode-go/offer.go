4dbbc22ac170dfb6d5e7c7a6c7479135ee118d52
LongQue <1220756253@qq.com>
2020-04-06 18:13:10 +0800

    面试题21. 调整数组顺序使奇数位于偶数前面

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -744,6 +744,36 @@ func isNumber(s string) bool {
 	return err == nil || errors.Is((interface{}(err).(*strconv.NumError)).Err, strconv.ErrRange)
 }
 
+/**
+面试题21. 调整数组顺序使奇数位于偶数前面
+输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数位于数组的前半部分，所有偶数位于数组的后半部分。
+
+示例：
+输入：nums = [1,2,3,4]
+输出：[1,3,2,4]
+注：[3,1,2,4] 也是正确的答案之一。
+
+提示：
+1 <= nums.length <= 50000
+1 <= nums[i] <= 10000
+*/
+func exchange(nums []int) []int {
+
+	for a, b := 0, len(nums)-1; ; {
+		for a < b && (nums[a]&1 == 1) {
+			a++
+		}
+		for a < b && (nums[b]&1 == 0) {
+			b--
+		}
+		if a < b {
+			nums[a], nums[b] = nums[b], nums[a]
+		} else {
+			return nums
+		}
+	}
+}
+
 func main() {
 	println(cuttingRope2(120))
 
