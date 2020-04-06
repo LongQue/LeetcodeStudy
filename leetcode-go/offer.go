be3b9e43f46b4b864e64521418ab07fe18f50ef6
LongQue <1220756253@qq.com>
2020-04-06 20:59:28 +0800

    面试题25. 合并两个排序的链表

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -821,6 +821,47 @@ func reverseList(head *ListNode) *ListNode {
 	return pre
 }
 
+/**
+面试题25. 合并两个排序的链表
+输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+
+示例1：
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+
+限制：
+0 <= 链表长度 <= 1000
+注意：本题与主站 21 题相同：https://leetcode-cn.com/problems/merge-two-sorted-lists/
+*/
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	cur := dummy
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			cur.Next = &ListNode{
+				Val:  l1.Val,
+				Next: nil,
+			}
+			l1 = l1.Next
+		} else {
+			cur.Next = &ListNode{
+				Val:  l2.Val,
+				Next: nil,
+			}
+			l2 = l2.Next
+		}
+		cur = cur.Next
+	}
+
+	if l1 != nil {
+		cur.Next = l1
+	}
+	if l2 != nil {
+		cur.Next = l2
+	}
+	return dummy.Next
+}
+
 func main() {
 	println(cuttingRope2(120))
 
