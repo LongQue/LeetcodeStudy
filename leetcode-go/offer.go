0c79501d411cb6f0473c8e6e02380bed66abd77c
LongQue <1220756253@qq.com>
2020-04-06 20:23:16 +0800

    24.反转链表 先定义next存cur的下一个节点，cur.next指向pre，pre移动到cur，cur移动到next

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -798,6 +798,29 @@ func getKthFromEnd(head *ListNode, k int) *ListNode {
 	return second
 }
 
+/**
+面试题24. 反转链表
+定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+
+示例:
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+
+限制：
+0 <= 节点个数 <= 5000
+*/
+func reverseList(head *ListNode) *ListNode {
+	var pre, next *ListNode
+	cur := head
+	for cur != nil {
+		next = cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+	return pre
+}
+
 func main() {
 	println(cuttingRope2(120))
 
