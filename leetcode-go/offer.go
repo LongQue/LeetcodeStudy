b620826d831f34db1e4bc7704b27d4e956d87375
LongQue <1220756253@qq.com>
2020-04-07 22:45:27 +0800

    面试题27. 二叉树的镜像

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -922,6 +922,42 @@ func isSubStructure2(A *TreeNode, B *TreeNode) bool{
 	return isSubStructure2(A.Left,B.Left)&&isSubStructure2(A.Right,B.Right)
 }
 
+/**
+面试题27. 二叉树的镜像
+请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+例如输入：
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+镜像输出：
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+示例 1：
+输入：root = [4,2,7,1,3,6,9]
+输出：[4,7,2,9,6,3,1]
+限制：
+0 <= 节点个数 <= 1000
+ */
+func mirrorTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	root.Left,root.Right = root.Right,root.Left
+	root.Left=mirrorTree(root.Left)
+	root.Right=mirrorTree(root.Right)
+	return root
+}
+
+
 func main() {
 	println(cuttingRope2(120))
 
