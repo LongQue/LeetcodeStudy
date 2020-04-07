8277b811e89b7fef664df2bb72bb0e521507d7d2
LongQue <1220756253@qq.com>
2020-04-07 22:40:12 +0800

    面试题26. 树的子结构

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -862,6 +862,66 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	return dummy.Next
 }
 
+/**
+面试题26. 树的子结构
+输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+
+例如:
+给定的树 A:
+     3
+    / \
+   4   5
+  / \
+ 1   2
+给定的树 B：
+
+   4
+  /
+ 1
+返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
+
+示例 1：
+
+输入：A = [1,2,3], B = [3,1]
+输出：false
+示例 2：
+
+输入：A = [3,4,5,1,2], B = [4,1]
+输出：true
+限制：
+
+0 <= 节点个数 <= 10000
+*/
+func isSubStructure(A *TreeNode, B *TreeNode) bool {
+	result := false
+	if A != nil && B != nil {
+		if A.Val == B.Val {
+			result=isSubStructure2(A,B)
+		}
+		if !result {
+			result = isSubStructure(A.Left, B)
+		}
+		if !result {
+			result = isSubStructure(A.Right, B)
+		}
+	}
+	return result
+}
+/* 从该节点开始判断是否是子结构*/
+func isSubStructure2(A *TreeNode, B *TreeNode) bool{
+	if B== nil {
+		return true
+	}
+	if A== nil {
+		return false
+	}
+	if A.Val !=B.Val {
+		return false
+	}
+	return isSubStructure2(A.Left,B.Left)&&isSubStructure2(A.Right,B.Right)
+}
+
 func main() {
 	println(cuttingRope2(120))
 
