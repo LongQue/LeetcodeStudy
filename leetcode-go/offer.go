b1aeda622a89210a51161abf24a847dcbec59c58
LongQue <1220756253@qq.com>
2020-04-12 13:23:17 +0800

    面试题33. 二叉搜索树的后序遍历序列

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -1291,6 +1291,49 @@ func levelOrder3(root *TreeNode) [][]int {
 	}
 	return result
 }
+
+/**
+面试题33. 二叉搜索树的后序遍历序列
+输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。如果是则返回 true，否则返回 false。假设输入的数组的任意两个数字都互不相同。
+
+参考以下这颗二叉搜索树：
+     5
+    / \
+   2   6
+  / \
+ 1   3
+
+示例 1：
+输入: [1,6,3,2,5]
+输出: false
+
+示例 2：
+输入: [1,3,2,6,5]
+输出: true
+
+提示：
+数组长度 <= 1000
+*/
+func verifyPostorder(postorder []int) bool {
+	if len(postorder) <= 1 {
+		return true
+	}
+
+	root := postorder[len(postorder)-1]
+	i := 0
+	for postorder[i] < root {
+		i++
+	}
+	mid := i
+	for ; i < len(postorder)-1; i++ {
+		if postorder[i] < root {
+			return false
+		}
+	}
+	return verifyPostorder(postorder[:mid]) && verifyPostorder(postorder[mid:len(postorder)-1])
+
+}
+
 func main() {
 	a := Constructor1()
 	a.Push(2)
