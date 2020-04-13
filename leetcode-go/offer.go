478a633f1a10429fc3a6b4f6aef81441ef77793d
LongQue <1220756253@qq.com>
2020-04-13 22:26:50 +0800

    面试题34 二叉树中和为某一值的路径

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -1334,19 +1334,56 @@ func verifyPostorder(postorder []int) bool {
 
 }
 
+/**
+面试题34 二叉树中和为某一值的路径
+输入一棵二叉树和一个整数，打印出二叉树中节点值的和为输入整数的所有路径。从树的根节点开始往下一直到叶节点所经过的节点形成一条路径。
+示例:
+给定如下二叉树，以及目标和 sum = 22，
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+返回:
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+深度遍历,注意sum未必为正，和节点值之间没有大于或小于关系，相加操作的结果可能在sum上下徘徊
+*/
+func pathSum(root *TreeNode, sum int) [][]int {
+	var result [][]int
+	pathSumWork(root, sum, 0, []int{}, &result)
+	return result
+}
+func pathSumWork(root *TreeNode, sum, now int, temp []int, result *[][]int) {
+	if root == nil || sum < now+root.Val {
+		return
+	}
+	now = now + root.Val
+	temp = append(temp, root.Val)
+	if now == sum && root.Left == nil && root.Right == nil {
+		tmp := make([]int, len(temp))
+		copy(tmp, temp)
+		*result = append(*result, tmp)
+		return
+	}
+	pathSumWork(root.Left, sum, now, temp, result)
+	pathSumWork(root.Right, sum, now, temp, result)
+}
+
 func main() {
-	a := Constructor1()
-	a.Push(2)
-	a.Push(0)
-	a.Push(3)
-	a.Push(0)
-	println(a.Min())
-	a.Pop()
-	println(a.Min())
-	a.Pop()
-	println(a.Min())
-	a.Pop()
-	println(a.Min())
+	i := TreeNode{
+		Val: 2,
+	}
+	a := TreeNode{
+		Val:  1,
+		Left: &i,
+	}
+	sum := pathSum(&a, 1)
+	println(sum)
 
 }
 
