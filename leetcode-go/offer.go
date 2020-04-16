8b5fe918aa55df45efa55f41113bcb892f4cd1e7
LongQue <1220756253@qq.com>
2020-04-16 23:42:18 +0800

    面试题40. 最小的k个数
    top k问题本想着写最大堆存最小值，但不会写，还是快排找第k个位置，终止排序，然后返回前k

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -1439,17 +1439,76 @@ func majorityElement(nums []int) int {
 	}
 	return result
 }
-func main() {
-	i := TreeNode{
-		Val: 2,
-	}
-	a := TreeNode{
-		Val:  1,
-		Left: &i,
-	}
-	sum := pathSum(&a, 1)
-	println(sum)
 
+/**
+面试题40. 最小的k个数
+输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+
+示例 1：
+输入：arr = [3,2,1], k = 2
+输出：[1,2] 或者 [2,1]
+
+示例 2：
+输入：arr = [0,1,2,1], k = 1
+输出：[0]
+
+限制：
+0 <= k <= arr.length <= 10000
+0 <= arr[i] <= 10000
+*/
+func getLeastNumbers(arr []int, k int) []int {
+	if k == 0 {
+		return nil
+	}
+	f := func(arr []int, k int) {}
+	f = func(arr []int, k int) {
+		l, r := 0, len(arr)-1
+		base := arr[0]
+		for l != r {
+			for base >= arr[l] && l < r {
+				l++
+			}
+			for base <= arr[r] && l < r {
+				r--
+			}
+			arr[l], arr[r] = arr[r], arr[l]
+		}
+		arr[0], arr[l-1] = arr[l-1], arr[0]
+		if r > k {
+			return
+		}
+
+	}
+
+}
+
+func getLeastNumbersWork(left, right, k int, arr []int) []int {
+	base := arr[left]
+	l, r := left, right
+	for {
+		for base >= arr[l] && l < r {
+			l++
+		}
+		for base <= arr[r] && l < r {
+			r--
+		}
+		if l == r {
+			arr[left], arr[l-1] = arr[l-1], arr[left]
+			break
+		}
+		arr[l], arr[r] = arr[r], arr[l]
+	}
+	if r > k {
+		getLeastNumbersWork()
+	}
+}
+
+func main() {
+	arr := []int{0, 1, 2, 3, 4, 0, 3, 3, 8, 1, 4, 6, 2, 8, 8, 15, 10, 0, 9, 9, 1, 2, 17, 8, 17, 25, 18, 18, 16, 13, 18, 29, 2, 3, 32, 2, 26, 23, 18, 8, 34, 8, 11, 36, 36, 39, 46, 30, 21, 25, 21, 14, 41, 10, 31, 55, 45, 16, 33, 47, 4, 52, 59, 60, 1, 43, 42, 10, 12, 56, 12, 27, 22, 52, 38, 12, 41, 42, 71, 5, 42, 76, 8, 3, 31, 65, 11, 29, 28, 68, 33, 50, 73, 87, 22, 68, 31, 1, 38, 89}
+	numbers := getLeastNumbers(arr, 60)
+	for _, v := range numbers {
+		print(v)
+	}
 }
 
 //Definition for singly-linked list.
