8d1ff0229b7b0425d1f0d3333549abe6c5ae192a
LongQue <1220756253@qq.com>
2020-04-23 23:54:52 +0800

    面试题52. 两个链表的第一个公共节点

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -2026,6 +2026,50 @@ func reversePairsMerge(leftArray, rightArray []int, count *int) []int {
 	}
 	return temp
 }
+
+/**
+面试题52. 两个链表的第一个公共节点
+输入两个链表，找出它们的第一个公共节点。
+
+示例 1：
+输入：listA = [4,1,8,4,5], listB = [5,0,1,8,4,5],
+listA 1 和listB 1都指向8，845为相同的节点
+输出：Reference of the node with value = 8
+输入解释：相交节点的值为 8 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+
+
+示例 2：
+输入：intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+输出：Reference of the node with value = 2
+输入解释：相交节点的值为 2 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4]。在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+
+示例 3：
+输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+输出：null
+输入解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+解释：这两个链表不相交，因此返回 null。
+
+*/
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+	node1 := headA
+	node2 := headB
+	for node1 != node2 {
+		if node1 != nil {
+			node1 = node1.Next
+		} else {
+			node1 = headB
+		}
+		if node2 != nil {
+			node2 = node2.Next
+		} else {
+			node2 = headA
+		}
+	}
+	return node1
+}
 func main() {
 	nums := []int{7, 5, 6, 4}
 	reversePairs(nums)
