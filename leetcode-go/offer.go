42668cbdc5be651c8e1739aeff6666bd118abe06
LongQue <1220756253@qq.com>
2020-04-26 22:52:14 +0800

    面试题58 - I. 翻转单词顺序

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -2461,8 +2461,73 @@ func findContinuousSequence(target int) [][]int {
 	return result
 }
 
+/**
+面试题58 - I. 翻转单词顺序
+输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，
+则输出"student. a am I"。
+
+示例 1：
+输入: "the sky is blue"
+输出: "blue is sky the"
+
+示例 2：
+输入: "  hello world!  "
+输出: "world! hello"
+解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+
+示例 3：
+输入: "a good   example"
+输出: "example good a"
+解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+
+说明：
+无空格字符构成一个单词。
+输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+
+双指针法
+先去头去尾，都从右边遍历，找到一个单词后完整复制
+*/
+func reverseWords(s string) string {
+	if s == "" {
+		return s
+	}
+	b := []byte(s)
+	left, right := 0, len(s)-1
+	for left <= right && (b[left] == ' ' || b[right] == ' ') {
+		if b[left] == ' ' {
+			left++
+		}
+		if b[right] == ' ' {
+			right--
+		}
+	}
+	if left > right {
+		return ""
+	}
+	b = b[left : right+1]
+	left, right = len(b)-1, len(b)-1
+	var result []byte
+	for left >= 0 {
+		if b[left] == ' ' && b[right] != ' ' {
+			println(string(b[left+1 : right+1]))
+			result = append(result, b[left+1:right+1]...)
+			result = append(result, ' ')
+			right = left
+		}
+		if b[left] != ' ' && b[right] == ' ' {
+			right = left
+		}
+		left--
+	}
+	result = append(result, b[left+1:right+1]...)
+	return string(result)
+}
 func main() {
-	findContinuousSequence(9)
+	s := " "
+	words := reverseWords(s)
+	println(len(words))
+	println(words)
 }
 
 //Definition for singly-linked list.
