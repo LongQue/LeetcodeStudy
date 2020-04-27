1f15a35b94a99856df1a34e6a1a26e8802edbd1c
LongQue <1220756253@qq.com>
2020-04-27 21:37:10 +0800

    面试题60. n个骰子的点数

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -2654,10 +2654,54 @@ func (this *MaxQueue) Pop_front() int {
 	return result
 }
 
+/**
+面试题60. n个骰子的点数
+把n个骰子扔在地上，所有骰子朝上一面的点数之和为s。输入n，打印出s的所有可能的值出现的概率。
+你需要用一个浮点数数组返回答案，其中第 i 个元素代表这 n 个骰子所能掷出的点数集合中第 i 小的那个的概率。
+
+示例 1:
+输入: 1
+输出: [0.16667,0.16667,0.16667,0.16667,0.16667,0.16667]
+
+示例 2:
+输入: 2
+输出: [0.02778,0.05556,0.08333,0.11111,0.13889,0.16667,0.13889,0.11111,0.08333,0.05556,0.02778]
+
+限制：
+1 <= n <= 11
+
+m代表 1个骰子时 1-6的次数 [1,1,1,1,1,1]
+m1代表 1+1个骰子时， 1-12的次数 [0,1....]
+之后m=m1，继续计算多一个骰子时m1的次数
+*/
+func twoSum60(n int) []float64 {
+	t := n
+	m := map[int]int{0: 1}
+	for t > 0 {
+		m1 := map[int]int{}
+		for i := 1; i <= 6; i++ {
+			for k, v := range m {
+				m1[k+i] += v
+			}
+		}
+		m = m1
+		t--
+	}
+	sum := 0
+	for _, v := range m {
+		sum += v
+	}
+	ret := make([]float64, n*6-n+1)
+	for k, v := range m {
+		ret[k-n] = float64(v) / float64(sum)
+	}
+	return ret
+}
+
 func main() {
-	i := []int{9, 10, 9, -7, -4, -8, 2, -6}
-	b := i[:0]
-	println(b)
+	i := make([]int, 12)
+	i = append(i, 2)
+	println(i)
 }
 
 //Definition for singly-linked list.
