2259b232bacd3a0084683479ca00458e98039c1f
LongQue <1220756253@qq.com>
2020-04-28 23:08:51 +0800

    面试题66. 构建乘积数组

--- leetcode-go/offer.go
+++ leetcode-go/offer.go
@@ -2865,8 +2865,41 @@ func add(a int, b int) int {
 	}
 	return a
 }
+
+/**
+面试题66. 构建乘积数组
+给定一个数组 A[0,1,…,n-1]，请构建一个数组 B[0,1,…,n-1]，其中 B 中的元素 B[i]=A[0]×A[1]×…×A[i-1]×A[i+1]×…×A[n-1]。不能使用除法。
+
+示例:
+输入: [1,2,3,4,5]
+输出: [120,60,40,30,24]
+
+提示：
+所有元素乘积之和不会溢出 32 位整数
+a.length <= 100000
+
+从头和尾两次，每次都避开乘原本位置得数
+*/
+func constructArr(a []int) []int {
+	if len(a) == 0 {
+		return a
+	}
+	ret := make([]int, len(a))
+	ret[0] = 1
+	for i := 1; i <= len(a)-1; i++ {
+		ret[i] = ret[i-1] * a[i-1]
+	}
+
+	temp := 1
+	for i := len(a) - 2; i >= 0; i-- {
+		temp *= a[i+1]
+		ret[i] *= temp
+	}
+	return ret
+}
 func main() {
-	print(add(-1, 2))
+	a := []int{1, 2, 3, 4, 5}
+	constructArr(a)
 
 }
 
