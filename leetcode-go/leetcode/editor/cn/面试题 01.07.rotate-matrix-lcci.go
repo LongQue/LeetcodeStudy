4c379b9637a08b08ae513e4a83f99f4d14a898b8
LongQue <1220756253@qq.com>
2020-05-21 22:09:21 +0800

    [面试题 01.07]旋转矩阵

---
+++ leetcode-go/leetcode/editor/cn/面试题 01.07.rotate-matrix-lcci.go
@@ -0,0 +1,67 @@
+package main
+
+/*
+[面试题 01.07]旋转矩阵
+//给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+//
+// 不占用额外内存空间能否做到？
+//
+//
+//
+// 示例 1:
+//
+// 给定 matrix =
+//[
+//  [1,2,3],
+//  [4,5,6],
+//  [7,8,9]
+//],
+//
+//原地旋转输入矩阵，使其变为:
+//[
+//  [7,4,1],
+//  [8,5,2],
+//  [9,6,3]
+//]
+//
+//
+// 示例 2:
+//
+// 给定 matrix =
+//[
+//  [ 5, 1, 9,11],
+//  [ 2, 4, 8,10],
+//  [13, 3, 6, 7],
+//  [15,14,12,16]
+//],
+//
+//原地旋转输入矩阵，使其变为:
+//[
+//  [15,13, 2, 5],
+//  [14, 3, 4, 1],
+//  [12, 6, 8, 9],
+//  [16, 7,10,11]
+//]
+//
+// Related Topics 数组
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func rotate(matrix [][]int) {
+	//90度翻转后  matrix[i][j]->matrix[j][n-i-1]
+
+	//先对角线互换，再每行中心对称互换
+	// 对角线 matrix[i][j] -> matrix[j][i]
+	// 中心对称互换  matrix[j][i] -> matrix[j][n-i-1]
+	n := len(matrix)
+	for i := 0; i < n; i++ {
+		for j := i; j < n; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+		for j := 0; j < n/2; j++ {
+			matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
+		}
+	}
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
