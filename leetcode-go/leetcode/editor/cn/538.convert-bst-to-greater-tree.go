492b3d79a22b9bc5dbfe9d42dcfccd8cb129bc94
LongQue <1220756253@qq.com>
2020-06-15 23:12:03 +0800

    [538]把二叉搜索树转换为累加树

---
+++ leetcode-go/leetcode/editor/cn/538.convert-bst-to-greater-tree.go
@@ -0,0 +1,57 @@
+package main
+
+/*
+[538]把二叉搜索树转换为累加树
+//给定一个二叉搜索树（Binary Search Tree），把它转换成为累加树（Greater Tree)，使得每个节点的值是原来的节点值加上所有大于它的节
+//点值之和。
+//
+//
+//
+// 例如：
+//
+// 输入: 原始二叉搜索树:
+//              5
+//            /   \
+//           2     13
+//
+//输出: 转换为累加树:
+//             18
+//            /   \
+//          20     13
+//
+//
+//
+//
+// 注意：本题和 1038: https://leetcode-cn.com/problems/binary-search-tree-to-greater-s
+//um-tree/ 相同
+// Related Topics 树
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+var sum int
+
+func convertBST(root *TreeNode) *TreeNode {
+	var add int
+	var f func(root *TreeNode)
+	f = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		f(root.Right)
+		root.Val += add
+		add = root.Val
+		f(root.Left)
+	}
+	f(root)
+	return root
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
