8cb19569b11996477c5ebaf446ae9938ae88ca45
LongQue <1220756253@qq.com>
2020-06-27 23:46:49 +0800

    [88]合并两个有序数组

---
+++ leetcode-go/leetcode/editor/cn/88.merge-sorted-array.go
@@ -0,0 +1,36 @@
+package main
+
+import "sort"
+
+/*
+[88]合并两个有序数组
+//给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+//
+//
+//
+// 说明:
+//
+//
+// 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+// 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+//
+//
+//
+//
+// 示例:
+//
+// 输入:
+//nums1 = [1,2,3,0,0,0], m = 3
+//nums2 = [2,5,6],       n = 3
+//
+//输出: [1,2,2,3,5,6]
+// Related Topics 数组 双指针
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	nums1 = append(nums1[:m], nums2[:n]...)
+	sort.Ints(nums1)
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
